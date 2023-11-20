819543a357fcab0b2fc9d843bd4ac66152236aa4
ollie-stytch <ollie@stytch.com>
2023-11-20 13:58:37 -0500

    Add server + hit server delete_user endpoint

---
+++ server/index.ts
@@ -0,0 +1,42 @@
+import dotenv from "dotenv";
+import express, { Express, Request, Response } from "express";
+import cors from "cors";
+import stytch, { UsersDeleteResponse } from "stytch";
+
+dotenv.config({path: '../.env.local'});
+
+const app: Express = express();
+
+app.use(express.json());
+app.use(cors());
+
+const client = new stytch.Client({
+    project_id: process.env.STYTCH_PROJECT_ID as string,
+    secret: process.env.STYTCH_SECRET as string,
+});
+
+interface DeleteUserReqInputs {
+    user_id: string,
+}
+
+app.post('/delete_user', (req: Request, res: Response) => {
+    console.log("hit!")
+    const { user_id }:DeleteUserReqInputs = req.body;
+
+    client.users.delete({user_id})
+        .then((resp: UsersDeleteResponse) => {
+            console.log(resp)
+            return res.status(200).json(resp)
+        })
+        .catch((err: any) => {
+            console.log(err)
+            return res.status(400).send(err)
+        });
+});
+
+const port = process.env.SERVER_PORT || 3001;
+
+app.listen(port, () => {
+    console.log(`Stytch passkey example app server listening on port ${port}`)
+});
+
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,6 +1,19 @@
 import { User } from "@stytch/core/public";
-// import { deleteUser } from "../lib/api";
 
+async function deleteUser(userId: string): Promise<any> {
+  try {
+    const response = await fetch('http://localhost:3001/delete_user', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({user_id: userId}),
+    });
+    return await response.json();
+  } catch (error) {
+    throw error;
+  }
+}
 // eslint-disable-next-line
 export const resetUserState = async (user: User | null, stytch: any) => {
   if (user === null) {
@@ -9,7 +22,7 @@ export const resetUserState = async (user: User | null, stytch: any) => {
 
   try {
     stytch.session.revoke();
-    // await deleteUser(user.user_id);
+    await deleteUser(user.user_id);
     window.location.href = "/login";
   } catch (e) {
     // eslint-disable-next-line no-console
